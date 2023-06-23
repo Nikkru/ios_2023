b27666bd24ca015de9f39b6dd565622ddb6d784d
NikNik <nickkru@gmail.com>
2023-06-23 13:59:13 +0300

    task 1

---
+++ krusser_hw_1.playground/Pages/hw_2.xcplaygroundpage/Contents.swift
@@ -0,0 +1,18 @@
+//: [Previous](@previous)
+
+import Foundation
+
+// 1. Написать функцию, которая на вход принимает число: сумма, которую пользователь хочет положить на вклад, следующий аргумент это годовой процент, третий аргумент это срок Функция возвращает сколько денег получит пользователь по итогу.
+func getMoney(deposit: Double, interest: Double, period: Int) -> Double {
+    
+    var sum = deposit
+    for i in 1 ... period {
+        sum += sum / 100 * interest
+        print("in \(i) year your would get \(sum) coins")
+    }
+    return sum
+//     deposit / 100 * interest * period + deposit
+}
+
+let sum = getMoney(deposit: 100.0, interest: 10.0, period: 3)
+//: [Next](@next)
