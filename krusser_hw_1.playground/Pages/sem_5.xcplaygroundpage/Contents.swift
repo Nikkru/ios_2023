b97a10df1df635c9bf3e53f342a12f6a63328264
NikNik <nickkru@gmail.com>
2023-07-12 11:48:38 +0300

    finish sem_5

--- krusser_hw_1.playground/Pages/sem_5.xcplaygroundpage/Contents.swift
+++ krusser_hw_1.playground/Pages/sem_5.xcplaygroundpage/Contents.swift
@@ -1,16 +1,48 @@
 import Foundation
 
+protocol MachineProtocol{
+    func start()
+    func finish()
+}
+
 protocol MenuProtocol {
     var name: String { get }
     var cost: Double { get }
 }
 
 class Cafe {
-    private var menus = [String]()
-    init(menus: [String]) {
-        self.menus = menus
+    var menu = [MenuProtocol]()
+    init(menu: [MenuProtocol]) {
+        self.menu = menu
     }
-//    func addMenu(menu: M)
+    func addMenu(newPosition: MenuProtocol) {
+        self.menu.append(newPosition)
+    }
+    func getMenu() {
+        menu.forEach { a in
+            print("\(a.name) - \(a.cost) рублей")
+        }
+    }
+    func getEnableMenu(deposit: Double) -> [MenuProtocol] {
+        print("\nДоступные Вам блюда до \(deposit) рублей:")
+        var enableMenu = menu.filter { dish in
+            dish.cost <= deposit
+        }
+        enableMenu.forEach { a in
+            print("\(a.name) - \(a.cost) рублей")
+        }
+        return enableMenu
+    }
+}
+
+struct Limonad: MenuProtocol {
+    var name: String
+    var cost: Double
+}
+
+struct Salad: MenuProtocol {
+    var name: String
+    var cost: Double
 }
 
 struct Tea: MenuProtocol {
@@ -24,8 +56,38 @@ struct Tea: MenuProtocol {
     var cost: Double
 }
 
+class FirstCafe: Cafe {
+    override func addMenu(newPosition: MenuProtocol) {
+        menu.append(newPosition)
+        print(newPosition)
+    }
+}
+
+final class VedingMashin: FirstCafe {
+    func getDish(num: Int, deposit: Double) -> MenuProtocol? {
+        guard menu[num].cost <= deposit && menu.count < num else { return nil }
+        return menu[num]
+    }
+}
 
 var green = Tea(type: .green, cost: 100)
+var cezar = Salad(name: "Cezar", cost: 120)
 green.name
 
+var oneCafe = Cafe(menu: [green])
+oneCafe.addMenu(newPosition: cezar)
+oneCafe.getMenu()
 
+oneCafe.getEnableMenu(deposit: 100)
+
+var veding1 = VedingMashin(menu: [green, cezar])
+var dish1 = veding1.getDish(num: 0, deposit: 130)
+
+extension VedingMashin: MachineProtocol {
+    func start() {
+        print("On")
+    }
+    func finish() {
+        print("Out")
+    }
+}
